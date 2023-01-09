c2f5307ac51cac7f4750cf1effc9a70396c179dd
michaelmensa <michaelmensa@github.com>
2023-01-09 14:51:04 +0000

    task 5 modified

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(len(my_string) - 2):
-        if (my_string[i] == 'c' or my_string[i] == 'C'):
-            my_string = my_string[:i] + "" + my_string[i+1:]
+    for r in range(len(my_string) - 2):
+        if (my_string[r] == 'c' or my_string[r] == 'C'):
+            my_string = my_string[:r] + "" + my_string[r+1:]
     return (my_string)
