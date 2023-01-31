0ca583e1da8c8b4a055f84c6633d586d291848cb
michaelmensa <michaelmensa@github.com>
2023-01-31 16:31:15 +0000

    18th commit

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,20 +1,3 @@
 #!/usr/bin/python3
-
-'''
-
-    module contains function copy_list
-
-'''
-
-
 def copy_list(l):
-
-    '''
-
-    Function: returns a copy of a list
-
-    '''
-
-    old_l = l
-    new_l = old_l[:]
-    return new_l
+    return l[:]
