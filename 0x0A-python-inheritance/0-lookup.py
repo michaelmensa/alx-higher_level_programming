fec0209d3b435445bb0a7f7c4c12f942579e3b49
michaelmensa <michaelmensa@github.com>
2023-02-06 19:42:06 +0000

    first commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+'''
+
+Module - lookup
+
+'''
+
+
+def lookup(obj):
+
+    ''' 
+        function that returns the list of available attributes and 
+        methods of an object.
+
+        Return: a list object
+    '''
+
+    return dir(obj)
