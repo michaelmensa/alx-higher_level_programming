0624b6006effcf841244847252be37b623084cc2
michaelmensa <michaelmensa@github.com>
2023-02-06 20:54:59 +0000

    fourth commit

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+'''
+    Module - Only sub class of
+'''
+
+
+def inherits_from(obj, a_class):
+
+    '''
+        Function that returns True if the object is an instance of a class
+        that inherited (directly or indirectly) from the specified class;
+        otherwise False
+    '''
+
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False
