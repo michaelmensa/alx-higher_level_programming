7197f626331a06173397d721083fa871b80da300
michaelmensa <michaelmensa@github.com>
2023-02-06 21:08:14 +0000

    sixth commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+'''
+    Module 6-base_geometry
+'''
+
+
+class BaseGeometry():
+
+    ''' Class BaseGeometry '''
+
+    def area(self):
+
+        ''' method raises an Exception with message; area() is not
+            implemented.
+        '''
+        raise Exception("area_() is not implemented")
