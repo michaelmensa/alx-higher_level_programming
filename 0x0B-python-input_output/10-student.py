94fa62124bd88bec64eddbd6036698b8fab13964
michaelmensa <michaelmensa@github.com>
2023-02-07 23:27:10 +0000

    12th commit

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+    Module - Student to JSON
+'''
+
+
+class Student():
+    ''' Class student that defines a student by;
+        first_name, last_name, age
+    '''
+    def __init__(self, first_name, last_name, age):
+        ''' istantiation with special init method '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        ''' retrieves a dictionary representation of a Student instance '''
+        dic = {}
+        if type(attrs) is not list:
+            return self.__dict__
+        else:
+            for i in attrs:
+                if i in self.__dict__:
+                    dic[i] = self.__dict__[i]
+            return dic
