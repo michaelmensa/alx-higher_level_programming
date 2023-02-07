bbd3b300f669667f35029f4b0ac63ed8ee089c2c
michaelmensa <michaelmensa@github.com>
2023-02-07 21:54:49 +0000

    fourth commit

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+''' Module - To JSON string '''
+import json
+
+
+def to_json_string(my_obj):
+    ''' function that returns the JSON representation of an
+        object (string)
+    '''
+    return json.dumps(my_obj)
