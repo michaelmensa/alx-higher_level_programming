3e994d46614fecf077f00f19321de475a5780f8d
michaelmensa <michaelmensa@live.com>
2023-04-16 14:29:09 +0000

    rest of mandatory tasks commit

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+   A JS Script that prints the elements of an array in a single line.
+   Using for loop to get this done, however other loop can still work.
+*/
+const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const item of lang) {
+  console.log(item);
+}
