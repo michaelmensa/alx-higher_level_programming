4ffd203953ad966f56094e1a1898fb9a95c9c341
michaelmensa <michaelmensa@live.com>
2023-04-19 22:56:50 +0000

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+The script takes in an arg and displays all values in states
+where 'name' matches the arg from db 'hbtn_0e_0_usa'
+this time, getting rid of SQL injections
+'''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    '''
+    Áccess to the db and get the states.
+    '''
+
+    db = MySQLdb.connect(host='localhost', user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states \
+                WHERE name LIKE BINARY '%(name)s' \
+                ORDER BY states.id ASC""", {'name': argv[4]})
+    rows = cur.fetchall
+
+    if rows is not None:
+        for row in rows:
+            print(row)
