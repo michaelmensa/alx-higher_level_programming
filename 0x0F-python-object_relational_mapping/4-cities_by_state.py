bddb310e05a1941731b6ebdd90e689888978c40c
michaelmensa <michaelmensa@live.com>
2023-04-19 23:11:19 +0000

    task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+This script lists all cities from the db
+'''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    '''
+    Access db and get the cities
+    '''
+
+    db = MySQLdb.connect(host='localhost', user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+    with db.cursor() as cur:
+        cur.execute('''
+            SELECT
+                cities.id, cities.name, states.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.states_id = states.id
+            ORDER BY
+                cities.id ASC
+                ''')
+        rows = cur.fetchall()
+
+    if rows is not None:
+        for row in rows:
+            print(row)
+
