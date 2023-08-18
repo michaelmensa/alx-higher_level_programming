7622a7f4d2f3630d86d5ee503f4e798159cf603d
michaelmensa <michaelmensa@live.com>
2023-08-18 15:43:50 +0000

    task 4 update 1.0

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,28 +3,17 @@
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
-    ''' script that lists all cities from db hbtn_0e_4_usa
+if __name__ == "__main__":
+    ''' script that lists all cities from db htn_0e_4_usa
     '''
 
-    db = MySQLdb.connect(host='localhost', user=argv[1], port=3306,
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    query = 'SELECT * FROM cities\
+            ORDER BY cities.id ASC'
+    cur.execute(query)
+    cities = cur.fetchall()
 
-    with db.cursor() as cur:
-        cur.execute("""
-            SELECT
-                cities.id, cities.name, states.name
-            FROM
-                cities
-            JOIN
-                states
-            ON
-                cities.name = states.id
-            ORDER BY
-                cities.id ASC
-        """)
-        cities = cur.fetchall()
-
-    if cities is not None:
-        for city in cities:
-            print(city)
+    for city in cities:
+        print(city)
